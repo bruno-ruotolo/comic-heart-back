a268c3dd3d040c85baa43e28821b0c6fe68d9bcb
allanornel <allanornel@gmail.com>
2022-05-11 01:24:32 -0300

    feat: addProduct done, but it needs a test with AuthController and Token

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,7 +3,6 @@ import { ObjectId } from "mongodb";
 
 export async function getProduct(req, res) {
   const { id } = req.params;
-  if (!id) return res.status(400).send("Não foi enviado o id no parametro");
   try {
     const product = await db
       .collection("products")
@@ -18,5 +17,40 @@ export async function getProduct(req, res) {
     res
       .status(500)
       .send("Falha no getProduct, aconteceu o seguinte erro: " + e);
+  }
+}
+// TODO Preciso testar esse addProduct com users/cart já feito, para conseguir ver se a lógica está certa.
+export async function addProduct(req, res) {
+  const { id } = req.params;
+  try {
+    const product = await db
+      .collection("products")
+      .find({ _id: new ObjectId(id) })
+      .toArray();
+    if (product.length === 0)
+      return res
+        .status(404)
+        .send("O id enviado não foi encontrado no Banco de Dados");
+
+    const user = res.locals.user;
+    const cart = user.cart;
+    let findProduct = false;
+    for (let i = 0; i < cart.length; i++) {
+      if (cart[i].productId === id) {
+        cart[i].quant++;
+        verif = true;
+      }
+    }
+    if (!findProduct) {
+      cart.push({ productId: id, quant: 1 });
+    }
+    await db
+      .collection("users")
+      .updateOne({ _id: user._id }, { $set: { cart: cart } });
+    res.status(200).send("Cart atualizado!");
+  } catch (e) {
+    res
+      .status(500)
+      .send("Falha no addProduct, aconteceu o seguinte erro: " + e);
   }
 }
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getProduct } from "../controllers/productController.js";
+import { addProduct, getProduct } from "../controllers/productController.js";
 import { validaHeader } from "../middlewares/validaHeader.js";
 
 const productRouter = Router();
 
 productRouter.get("/product/:id", validaHeader, getProduct);
+productRouter.put("/addProduct/:id", validaHeader, addProduct);
 
 export default productRouter;
